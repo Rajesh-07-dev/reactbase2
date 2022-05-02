29d4a43b1a49e8dc59808f706672d01e5047d87d
Rajesh-07-dev <rajesh962689@gmail.com>
2022-05-02 10:19:29 +0530

    new app commit

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import './style.css';
 export default function App() {
   return (
     <div>
-      <h1>Hello raj</h1>
+      <h1>Hello new app</h1>
       <p>Start editing to see some magic happen :)</p>
     </div>
   );
